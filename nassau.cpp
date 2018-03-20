38930fccb143bdbd63ea1e5ea93431d356375807
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-03-20 20:48:52 +0100

    Tentativi

--- nassau.cpp
+++ nassau.cpp
@@ -5,21 +5,19 @@ using namespace std;
 #include <string>
 #include <vector>
 
-int alternative(int v, int f, int mv, int c) {
+int finals(int v, int f, int mv, int c) {
   if (c <= 0) {
-    return 0;
-  } else if (v <= 0 && mv <= 0 && f <= 0) {
-    return 0;
+    return 1;
   } else {
-    int res = v + mv + f;
+    int res = 0;
     if (v > 0) {
-      res += alternative(v - 1, f, mv + 1, c - 1);
+      res += finals(v - 1, f, mv + 1, c - 1) * v;
     }
     if (f > 0) {
-      res += alternative(v, f - 1, mv, c - 1);
+      res += finals(v, f - 1, mv, c - 1) * f;
     }
     if (mv > 0) {
-      res += alternative(v, f, mv - 1, c - 1);
+      res += finals(v, f, mv - 1, c - 1) * mv;
     }
     return res;
   }
@@ -35,7 +33,7 @@ int main() {
   int V, F, MV = 0, C;
   in >> V >> F >> C;
 
-  int res = alternative(V, F, MV, C);
+  int res = finals(V, F, MV, C);
 
   cout << res << endl;
 
