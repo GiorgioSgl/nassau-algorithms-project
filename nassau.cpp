732970d27b94276dfe4803f02cf62150bc0a141c
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-03-20 14:58:01 +0100

    Added memo

--- nassau.cpp
+++ nassau.cpp
@@ -1,66 +1,63 @@
 using namespace std;
-#include <cmath>
+
+#include <cstdlib>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <unordered_map>
-#include <vector>
 
-int memoDP(unordered_map<string, pair<int, int>> &DP, int v, int f, int P);
-int calcDP(unordered_map<string, pair<int, int>> &DP, int v, int f, int P);
-void solution(vector<int> &sol, int V, int F, int P);
+double potenzaPonderata(unordered_map<string, double> &DP, int v, int f, int mV,
+                        int colpi, double prob);
+double potenzaMemo(unordered_map<string, double> &DP, int v, int f, int mV,
+                   int colpi, double prob);
 
-string intToKey(int a, int b) { return to_string(a) + " " + to_string(b); }
-
-int memoDP(unordered_map<string, pair<int, int>> &DP, int v, int f, int P) {
-  string key = intToKey(v, f);
-  if (!DP.count(key)) {
-    DP[key].first = calcDP(DP, v, f, P);
-    DP[key].second = 1;
-  } else {
-    DP[key].second += 1;
-  }
-  return DP[key].first;
+string intToKey(int a, int b, int c) {
+  return to_string(a) + to_string(b) + to_string(c);
 }
 
-int calcDP(unordered_map<string, pair<int, int>> &DP, int v, int f, int P) {
-  int N = ((int)ceil(v / 2.0)) + f;
-  if (P == 0) {
-    return 0;
-  } else if (!(v > 0 && f > 0)) {
-    return 0;
+double potenzaPonderata(unordered_map<string, double> &DP, int v, int f, int mV,
+                        int colpi, double prob) {
+
+  if (colpi == 0) {
+    return prob * (f * (mV + v));
   } else {
-    return memoDP(DP, v - 1, f, P - 1) + memoDP(DP, v, f - 1, P - 1) + N;
+    double uno = 0, due = 0, tre = 0;
+    if (v != 0)
+      uno = potenzaMemo(DP, v - 1, f, mV + 1, colpi - 1,
+                        prob * ((double)v / (v + f + mV)));
+    if (f != 0)
+      due = potenzaMemo(DP, v, f - 1, mV, colpi - 1,
+                        prob * ((double)f / (v + f + mV)));
+    if (mV != 0)
+      tre = potenzaMemo(DP, v, f, mV - 1, colpi - 1,
+                        prob * ((double)mV / (v + f + mV)));
+
+    return uno + due + tre;
   }
 }
 
-void solution(vector<int> &sol, int V, int F, int P) {
-  int N = V + F;
-  unordered_map<string, pair<int, int>> DP;
-  int result = memoDP(DP, 2 * V, F, P);
-
-  for (pair<string, pair<int, int>> element : DP) {
-    cout << "Key   : " << element.first << endl;
-    cout << "First : " << element.second.first << endl;
-    cout << "Second: " << element.second.second << endl << endl;
+double potenzaMemo(unordered_map<string, double> &DP, int v, int f, int mV,
+                   int colpi, double prob) {
+  string key = intToKey(v, f, mV);
+  if (DP.count(key) == 0) {
+    DP[key] = potenzaPonderata(DP, v, f, mV, colpi, prob);
   }
+  return DP[key];
 }
 
 int main() {
 
   ifstream in("input.txt");
 
-  int V, F, P;
-  in >> V >> F >> P;
-  int N = V + F;
+  int v, f, c;
+  in >> v >> f >> c;
+  // cout << ((double)v/(f+v));
+  unordered_map<string, double> DP;
+  double res = potenzaPonderata(DP, v, f, 0, c, 1);
 
-  vector<int> sol;
-  solution(sol, V, F, P);
-
-  // double result;
-  // ofstream out("output.txt");
-  // out << scientific << setprecision(10) << result << endl;
+  ofstream out("output.txt");
+  out << scientific << setprecision(10) << res << endl;
 
   return 0;
 }
