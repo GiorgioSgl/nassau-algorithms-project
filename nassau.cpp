0bf4a64be064dd1ed312882ab4e3db66ac039da8
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-03-21 10:52:28 +0100

    Giorgio 90

--- nassau.cpp
+++ nassau.cpp
@@ -8,67 +8,55 @@ using namespace std;
 #include <unordered_map>
 
 double potenzaPonderata(unordered_map<string, double> &DP, int v, int f, int mV,
-                        int colpi, double prob);
+                        int colpi);
 double potenzaMemo(unordered_map<string, double> &DP, int v, int f, int mV,
-                   int colpi, double prob);
+                   int colpi);
 
-string intToKey(int a, int b, int c, double d) {
+string intToKey(int a, int b, int c, int d) {
   return to_string(a) + " " + to_string(b) + " " + to_string(c) + " " +
          to_string(d);
 }
 
 double potenzaPonderata(unordered_map<string, double> &DP, int v, int f, int mV,
-                        int colpi, double prob) {
+                        int colpi) {
   if ((v == 0 && mV == 0) || f == 0) {
     return 0;
   } else if (colpi == 0) {
-    return prob * (f * (mV + v));
+    return f * (mV + v);
   } else {
     double uno = 0, due = 0, tre = 0;
     if (v != 0)
-      uno = potenzaMemo(DP, v - 1, f, mV + 1, colpi - 1,
-                        prob * ((double)v / (v + f + mV)));
+      uno = ((double)v / (v + f + mV)) *
+            potenzaMemo(DP, v - 1, f, mV + 1, colpi - 1);
     if (f != 0)
-      due = potenzaMemo(DP, v, f - 1, mV, colpi - 1,
-                        prob * ((double)f / (v + f + mV)));
+      due =
+          ((double)f / (v + f + mV)) * potenzaMemo(DP, v, f - 1, mV, colpi - 1);
     if (mV != 0)
-      tre = potenzaMemo(DP, v, f, mV - 1, colpi - 1,
-                        prob * ((double)mV / (v + f + mV)));
+      tre = ((double)mV / (v + f + mV)) *
+            potenzaMemo(DP, v, f, mV - 1, colpi - 1);
 
     return uno + due + tre;
   }
 }
 
 double potenzaMemo(unordered_map<string, double> &DP, int v, int f, int mV,
-                   int colpi, double prob) {
-  string key = intToKey(v, f, mV, prob);
+                   int colpi) {
+  string key = intToKey(v, f, mV, colpi);
   if (DP.count(key) == 0) {
-    DP[key] = potenzaPonderata(DP, v, f, mV, colpi, prob);
+    DP[key] = potenzaPonderata(DP, v, f, mV, colpi);
   }
   return DP[key];
 }
 
 int main() {
 
-  // ifstream in("input.txt");
-  string id;
-  cin >> id;
-
-  ifstream in("dataset/input/input" + id + ".txt");
+  ifstream in("input.txt");
 
   int v, f, c;
   in >> v >> f >> c;
 
   unordered_map<string, double> DP;
-  double res = c >= ((2 * v) + f) ? 0 : potenzaPonderata(DP, v, f, 0, c, 1);
-
-  // int i = 0;
-  // for (pair<string, double> element : DP) {
-  //   cout << "Key     = " << element.first << endl;
-  //   cout << "DP[key] = " << element.second << endl;
-  //   i++;
-  // }
-  // cout << endl << "Num keys = " << i << endl;
+  double res = c >= (2 * v + f) ? 0 : potenzaPonderata(DP, v, f, 0, c);
 
   ofstream out("output.txt");
   out << scientific << setprecision(10) << res << endl;
