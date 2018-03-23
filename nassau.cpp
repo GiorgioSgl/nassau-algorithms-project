48f54b426c445ace4fb987825a41062523cbc7d0
Tommaso Bonomo <tommaso.bonomo.97@gmail.com>
2018-03-23 18:08:39 +0100

    Gives back accurate prob. within shots limit

--- nassau.cpp
+++ nassau.cpp
@@ -5,21 +5,24 @@ using namespace std;
 #include <string>
 #include <vector>
 
-int finals(int v, int f, int mv, int c) {
-  if (c <= 0) {
-    return (v + mv) * f;
+double takeDownProb(int v, int f, int V, int F, int colpi) {
+  if (v == 0 && f == 0) {
+    return 1;
+  } else if (2 * v + f >= colpi) {
+    return v * f;
   } else {
-    int res = 0;
+    double tookDownV = 0;
     if (v > 0) {
-      res += finals(v - 1, f, mv + 1, c - 1);
+      tookDownV = ((double)V / ((double)(V + F) * (V + F))) *
+                  takeDownProb(v - 1, f, V - 1, F, colpi - 2);
     }
+    double tookDownF = 0;
     if (f > 0) {
-      res += finals(v, f - 1, mv, c - 1);
+      tookDownF = ((double)F / ((double)V + F)) *
+                  takeDownProb(v, f - 1, V, F - 1, colpi - 1);
     }
-    if (mv > 0) {
-      res += finals(v, f, mv - 1, c - 1);
-    }
-    return res;
+
+    return tookDownV + tookDownF;
   }
 }
 
@@ -30,12 +33,13 @@ int main() {
   // cin >> id;
   // ifstream in("dataset/input/input" + id + ".txt");
 
-  int V, F, MV = 0, C;
+  int V, F, C;
   in >> V >> F >> C;
 
-  int res = finals(V, F, MV, C);
+  int i, j;
+  cin >> i >> j;
 
-  cout << res << endl;
+  cout << takeDownProb(i, j, V, F, C) << endl;
 
   return 0;
 }
